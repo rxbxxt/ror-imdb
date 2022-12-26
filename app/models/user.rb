8e9f95376e91dc0ce269fda860479f55dd4438c7
meow <nshyahn1@gmail.com>
2022-12-26 02:22:29 +0200

    Add roles [user, admin] to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :movies
+  enum role: [:user, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+  def set_default_role
+    self.role ||= :user
+  end
 end
