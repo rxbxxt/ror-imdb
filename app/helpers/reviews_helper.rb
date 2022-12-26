8894c63191b310be196f858511a40b359a42b897
meow <nshyahn1@gmail.com>
2022-12-26 05:26:43 +0200

    Add admin|user scope and reviews

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -2,4 +2,8 @@ module ReviewsHelper
   def can_comment
     !Review.exists?(user_id: current_user.id, movie_id: @movie.id)
   end
+
+  def commentator(review)
+    User.find(review.user_id).email
+  end
 end
